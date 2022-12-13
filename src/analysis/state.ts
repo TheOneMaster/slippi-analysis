77877bddbae1cdb9c009228f1b6c6f7d030ee628
Nayan Nair <25909677+TheOneMaster@users.noreply.github.com>
2022-12-13 09:44:10 +0530

    Recovery state identification

--- src/analysis/state.ts
+++ src/analysis/state.ts
@@ -1,7 +1,7 @@
 import { FrameEntryType, PostFrameUpdateType } from "@slippi/slippi-js";
 
 import { FrameStateError } from "../error";
-import { STAGE_GEOMETRY_MAP } from "./constants";
+import { STAGE_GEOMETRY_MAP } from "../base/constants";
 
 /*
 
@@ -57,7 +57,7 @@ export function isOnstage(frame: PostFrameUpdateType, stageId: number): boolean 
     const char_pos_x = frame.positionX;
     const char_pos_y = frame.positionY;
 
-    if (!char_pos_x || !char_pos_y) {
+    if (char_pos_x === null || char_pos_y === null) {
         throw new Error("Unable to get player position in frame")
     }
 
@@ -65,9 +65,13 @@ export function isOnstage(frame: PostFrameUpdateType, stageId: number): boolean 
     let within_y: boolean;
 
     if (stageGeometry.topPlatformHeight !== undefined) {
-        within_y = (stageGeometry.topPlatformHeight >= char_pos_y && char_pos_y >= stageGeometry.mainPlatformHeight);
+        const halfway_y = (stageGeometry.upperYBoundary - stageGeometry.topPlatformHeight) / 2;
+        const max_y = stageGeometry.topPlatformHeight + halfway_y;
+        within_y = (max_y >= char_pos_y && char_pos_y >= stageGeometry.mainPlatformHeight - 5);
     } else {
-        within_y = stageGeometry.mainPlatformHeight === char_pos_y;
+        const halfway_y = (stageGeometry.upperYBoundary - stageGeometry.mainPlatformHeight) / 2;
+        const max_y = stageGeometry.mainPlatformHeight + halfway_y;
+        within_y = (max_y >= char_pos_y && char_pos_y >= stageGeometry.mainPlatformHeight - 5);
     }
 
     return within_x && within_y
@@ -76,14 +80,47 @@ export function isOnstage(frame: PostFrameUpdateType, stageId: number): boolean 
 
 
 export function isRecovering(frame: PostFrameUpdateType, stageId: number) {
+    // Airborne, off-stage, and moving closer towards center (0, 0)
 
-    const isAirborne = frame.isAirborne;
+    const grounded = !frame.isAirborne;
+    const onStage = isOnstage(frame, stageId);
 
-    if (!isAirborne) {
+    if (grounded || onStage) {
         return false
     }
 
-    return isOnstage(frame, stageId) && isAirborne;
+    const momentum = frame.selfInducedSpeeds;
+
+    if (!momentum) {
+        throw new Error("Unable to obtain momentum from player on frame.")
+    }
+
+
+    // Get current position
+    const posX = frame.positionX;
+    const posY = frame.positionY;
+
+    if (posX === null || posY === null) {
+        throw new Error("Unable to obtain position");
+    }
+
+
+    // Get momentum
+    const momentumX = momentum.airX;
+    const momentumY = momentum.y;
+
+    if (momentumX === null || momentumY === null) {
+        throw new Error("Unable to obtain momentum");
+    }
+
+    // Make sure that the player is recovering towards the stage
+    const movedX = posX + momentumX;
+    const movedY = posY + momentumY;
+
+    const closerX = Math.abs(movedX) <= Math.abs(posX);
+    const closerY = Math.abs(movedY) <= Math.abs(posY);
+
+    return closerX || closerY
 }
 
 
