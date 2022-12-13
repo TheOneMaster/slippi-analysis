6cc9b8a439b980a261d611c60e913978eec177bc
Nayan Nair <25909677+TheOneMaster@users.noreply.github.com>
2022-12-13 09:42:58 +0530

    Added recovery identification function

--- src/analysis/combo.interface.ts
+++ src/analysis/combo.interface.ts
@@ -1,13 +1,29 @@
 export interface Combo {
     frames: number[],
     player: number,
+
     startFrame: number,
     endFrame: number,
+    
     startPercent: number,
     endPercent: number,
+    
     toDeath: boolean,
     fromGrab?: boolean
 }
-export interface ReplayCombos {
+export interface ReplayCombos { 
     [frameIndex: string]: Combo
+}
+
+export interface RecoveryObject {
+    startFrame: number,
+    endFrame: number,
+
+    startPercent: number,
+    endPercent: number,
+
+    successful: boolean
+}
+export interface GameRecoveries {
+    [playerIndex: number]: RecoveryObject[]
 }
--- src/analysis/combo.ts
+++ src/analysis/combo.ts
@@ -1,8 +1,10 @@
 import { SlippiGame } from "@slippi/slippi-js";
 
+import { SlippiReadError } from "../error";
+
 import { getGrabFrames } from "./base";
-import { Combo, ReplayCombos } from "./combo.interface";
-import { isInGroundedControl, isDead } from "./state";
+import { Combo, GameRecoveries, RecoveryObject, ReplayCombos } from "./combo.interface";
+import { isInGroundedControl, isDead, isRecovering } from "./state";
 
 export function getGrabCombos(slp: SlippiGame): ReplayCombos {
 
@@ -24,7 +26,7 @@ export function getGrabCombos(slp: SlippiGame): ReplayCombos {
         let frames: number[] = []
 
         // Iterate over every frame from the start of the combo and break when the combo ends
-        for (let frame_index=slp_frame; frame_index <= slp_frame_index.length; frame_index++) {
+        for (let frame_index = slp_frame; frame_index <= slp_frame_index.length; frame_index++) {
 
             const frame_data = slp_frames[frame_index];
             const player_data = frame_data.players[grabbed_player]?.post;
@@ -43,7 +45,7 @@ export function getGrabCombos(slp: SlippiGame): ReplayCombos {
                     frames: frames,
                     player: grab_init_frames[grab_frame].by,
                     startFrame: slp_frame,
-                    endFrame: frame_index-1,
+                    endFrame: frame_index - 1,
                     startPercent: init_percent ?? 0,
                     endPercent: final_percent ?? 0,
                     toDeath: false,
@@ -64,5 +66,87 @@ export function getGrabCombos(slp: SlippiGame): ReplayCombos {
     }
 
     return combos
+}
+
+export function getRecoveries(slp: SlippiGame) {
+
+    const slp_players = slp.getSettings()?.players;
+    const slp_stageId = slp.getSettings()?.stageId;
+
+    if (slp_players === undefined || slp_stageId == null) {
+        throw new SlippiReadError();
+    }
+
+
+    const slp_frames = slp.getFrames();
+
+    const GameRecoveries: GameRecoveries = {};
+    const player_recovery_storage: { [playerIndex: number]: RecoveryObject } = {};
+
+
+    for (const frame_index in slp_frames) {
+
+        const frame_data = slp_frames[frame_index];
+        const players = frame_data.players;
+
+
+        for (const playerId in players) {
+
+            const playerData = players[playerId]?.post;
+
+            if (playerData === undefined) {
+                continue
+            }
+
+            const recovering = isRecovering(playerData, slp_stageId);
+            const dead = isDead(playerData);
+            const playerId_int = parseInt(playerId);
+
+            if (!recovering || dead) {
+
+                if (!player_recovery_storage[playerId_int]) {
+                    continue
+                }
+
+                const recovery = player_recovery_storage[playerId_int];
+                delete player_recovery_storage[playerId_int];
+
+                if (dead) recovery.successful = false
+
+                if (!GameRecoveries[playerId_int]) {
+                    GameRecoveries[playerId_int] = [recovery]
+                } else {
+                    GameRecoveries[playerId_int].push(recovery);
+                }
+                continue
+            }
+
+
+            const frame_int = parseInt(frame_index);
+
+            if (!player_recovery_storage[playerId_int]) {
+                const recovery: RecoveryObject = {
+                    startFrame: frame_int,
+                    endFrame: frame_int,
+
+                    startPercent: playerData.percent ?? -1,
+                    endPercent: playerData.percent ?? -1,
+
+                    successful: true
+                }
+
+                player_recovery_storage[playerId_int] = recovery;
+            } else {
+                const recovery = player_recovery_storage[playerId_int];
+
+                recovery.endFrame = frame_int;
+                recovery.endPercent = playerData.percent ?? recovery.endPercent;
+            }
+
+
+        }
+    }
+
+    return GameRecoveries
 }
 
