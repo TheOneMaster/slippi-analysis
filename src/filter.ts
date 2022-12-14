107b846dc6a66d5c2a4b3250276df978dae91921
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2022-12-14 06:45:06 +0530

    Commented filter functions
    - Removed filter options from `filterGames` convenience function

--- src/filter.ts
+++ src/filter.ts
@@ -1,11 +1,16 @@
 import { Character, SlippiGame, Stage } from "@slippi/slippi-js";
 
 import { SlippiReadError } from "./error";
-import { Filter, FilterOptions } from "./filter.interface";
+import { Filter } from "./filter.interface";
 import { getCharacters } from "./analysis/char";
     
 
-
+/**
+ * Check whether the given characters were used in the replay file. Used in the filter function.
+ * @param slp - The Slippi replay file to be checked
+ * @param chars - The list of characters searched for
+ * @returns Boolean of whether the replay contains the given characters
+ */
 function checkCharacters(slp: SlippiGame, chars: Character[]): boolean {
 
     const slp_chars = getCharacters(slp);
@@ -26,6 +31,12 @@ function checkCharacters(slp: SlippiGame, chars: Character[]): boolean {
     return true;
 }
 
+/**
+ * Check for which stage the game was played on. To be used in the final filter function.
+ * @param slp - The Slippi replay file to be checked
+ * @param stage - The stage ID to be checked
+ * @returns Boolean of whether the game was played on the correct stage
+ */
 function checkStage(slp: SlippiGame, stage: Stage): boolean {
 
     const settings = slp.getSettings();
@@ -38,6 +49,12 @@ function checkStage(slp: SlippiGame, stage: Stage): boolean {
     return stageId === stage;
 }
 
+/**
+ * Check whether the replay file contains a certain number of players. Used as a part of the filter function. 
+ * @param slp - Slippi replay file
+ * @param num_players - The number of players in the game (Ex: 2 for 1v1)
+ * @returns Boolean for whether the replay contains the same number of players
+ */
 function checkNumPlayers(slp: SlippiGame, num_players: number) {
     const settings = slp.getSettings();
     const players = settings?.players;
@@ -47,7 +64,13 @@ function checkNumPlayers(slp: SlippiGame, num_players: number) {
     return players.length === num_players;
 }
 
-
+/**
+ * Check whether provided replay file matches the filters
+ * 
+ * @param slp - Slippi replay file
+ * @param filters - The object containing the filters used to check against the provided replay
+ * @returns Boolean of whether the replay matches the filters
+ */
 export function checkFilters(slp: SlippiGame, filters: Filter): boolean {
 
     let final_bool = true;
@@ -59,7 +82,16 @@ export function checkFilters(slp: SlippiGame, filters: Filter): boolean {
     return final_bool
 }
 
-export function filterGames(slp_list: SlippiGame[], filter_game: SlippiGame, filter_options?: FilterOptions): SlippiGame[] {
+/**
+ * Convenience function to find similar games to the provided filter_game. Filters on the basis of characters, stage, and number of players.
+ * 
+ * @param slp_list - The list of slp files to filter from
+ * @param filter_game - The game the filter is based on
+ * 
+ * @returns Filtered list of slp files
+ * 
+ */
+export function filterGames(slp_list: SlippiGame[], filter_game: SlippiGame): SlippiGame[] {
 
     const chars = getCharacters(filter_game)
     const settings = filter_game.getSettings();
@@ -71,17 +103,11 @@ export function filterGames(slp_list: SlippiGame[], filter_game: SlippiGame, fil
         throw new SlippiReadError();
     }
 
-    const filter: Filter = {};
-
-    if (filter_options) {
-        if (filter_options.chars) filter.chars = chars;
-        if (filter_options.stage) filter.stage = stage;
-        if (filter_options.numPlayers) filter.numPlayers = numPlayers;
-    } else {
-        filter.chars = chars;
-        filter.stage = stage;
-        filter.numPlayers = numPlayers;
-    }
+    const filter: Filter = {
+        chars: chars,
+        stage: stage,
+        numPlayers: numPlayers
+    };
 
     const filtered_games = slp_list.filter(slp => checkFilters(slp, filter));
 
